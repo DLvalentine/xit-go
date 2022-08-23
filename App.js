0109a15ac5566541eb372db7f28ba8655c64aa9a
David L Valentine <davidlewisvalentine@gmail.com>
2022-08-23 12:47:25 -0700

    Stubbed out raw editor

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import RawEditor from './views/RawEditor';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>First thing's first - let's create a raw view</Text>
-    </View>
+    <NavigationContainer documentTitle={{
+      formatter: (options, route) =>
+        `${options?.title ?? route?.name} - Xit-Go`,
+    }}>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        {/*<Tab.Screen name="Visual Editor" component={VisualEditor} options={{tabBarIcon: ({color,size}) => <MaterialCommunityIcons name="eye" color={color} size={size} />}} />*/}
+        <Tab.Screen name="Raw Editor" component={RawEditor} options={{tabBarIcon: ({color,size}) => <MaterialCommunityIcons name="code-braces" color={color} size={size} />}} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # xit-go
-Mobile-Friendly [Xit File](https://xit.jotaen.net/) Frontend, built with [React Native + Expo Go](https://reactnative.dev/)
+Mobile-Friendly [Xit File](https://xit.jotaen.net/) Editor, built with: [React Native](https://reactnative.dev/), [React Native Navigation](https://reactnavigation.org/docs/getting-started/).
