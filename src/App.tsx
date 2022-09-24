67e49c5702d0724d4fa4be573d46072090161fe7
David L Valentine <davidlewisvalentine@gmail.com>
2022-09-24 15:02:48 -0700

    More basics, getting file read/write working...

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,12 @@ import {
   IonFab, 
   IonFabButton,
   setupIonicReact,
+  IonRow,
+  IonCol,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { getPlatforms } from '@ionic/react';
-import { cloudUploadOutline, code, eye } from 'ionicons/icons';
+import { folderOpenOutline, code, eye, download } from 'ionicons/icons';
 import { FilePicker } from '@capawesome/capacitor-file-picker';
 
 // Pages/Components
@@ -58,12 +60,16 @@ setupIonicReact({
 });
 
 // TODO -> docs
+// TODO -> desktop keyboard shortcuts?
+// TODO -> better handling for NEW files? (btn?)
 const App: React.FC = () => {
+  const [fileName, setFileName] = useState(null);
   const [filePath, setFilePath] = useState(null);
   const [fileRaw, setFileRaw] = useState(null);
   const [fileObject, setFileObject] = useState(null);
   // TODO -> Error object/state for toast messages
   // TODO -> Autosave state for mobile/electron (default false)
+  // TODO -> ^ related, timer for autosaving
 
   return (
     <IonApp>
@@ -74,7 +80,7 @@ const App: React.FC = () => {
               <Visual />
             </Route>
             <Route exact path="/raw">
-              <Raw />
+              <Raw fileName={fileName} fileRaw={fileRaw} setFileRaw={setFileRaw} setFileObject={setFileObject} />
             </Route>
             <Route exact path="/">
               <Redirect to="/visual" />
@@ -93,31 +99,50 @@ const App: React.FC = () => {
         </IonTabs>
       </IonReactRouter>
       <IonFab vertical="top" horizontal="end" slot="fixed">
-        <IonFabButton id="file-picker-button" onClick={() => pickFileHandler(setFilePath, setFileRaw, setFileObject)}>
-          <IonIcon icon={cloudUploadOutline} />
-        </IonFabButton>
+        <IonRow>
+          <IonCol>
+            <IonFabButton id="pick-file-btn" onClick={() => pickFileHandler(setFileName, setFilePath, setFileRaw, setFileObject)}>
+              <IonIcon icon={folderOpenOutline} />
+            </IonFabButton>
+          </IonCol>
+          <IonCol>
+            {/* TODO -> disable save? How do we handle NEW files */}
+            <IonFabButton id="save-file-btn" onClick={() => saveFileHandler(fileName || '', fileRaw || '')}>
+              <IonIcon icon={download} />
+            </IonFabButton>
+          </IonCol>
+        </IonRow>
       </IonFab>
     </IonApp>
   );
 };
 
+// TODO -> if autosave, set the timeout (in state for it too) to autosave while the file is open
 // TODO -> usage of xitParse kinda sucks, should rename the default export to xitParse
 // TODO -> docs
+// TODO -> types, interfaces for things if needed
 // NOTE/TODO -> For below, one of the other things to consider is the auto-save vs. manual save question... when/how/UI consideration. We aren't going to do
 //              tabbed editing at first, so think "notepad" before we get to "notepad++" I think manual saving is fine... but it could be too ez to accidentally blow away changes.
 //              See below for more.
-const pickFileHandler = async (setFilePath: Function, setFileRaw: Function, setFileObject: Function) => {
+// NOTE/TODO -> FileSystem API has some good stuff for web, not sure if we want to circle back and rewrite this or not.
+const pickFileHandler = async (setFileName: Function, setFilePath: Function, setFileRaw: Function, setFileObject: Function) => {
   const platforms : Array<string> = getPlatforms();
 
   // Use Electron APIs for file i/o if platform is electron.
   if(platforms.includes('electron')) {
     // TODO -> implement when ready to work on Desktop
     // NOTE -> The below works fine for web/mobile, but web is going to need some extra help for file saving lol... saving is going to be a wild ride, too - for web we'll need to download instead of just.... save. ugh
+    // TODO -> handle desktop differently for saving and all that
   } else {
     await FilePicker.pickFiles({ types: [], readData: true, multiple: true }).then((results: any) => {
       try {
+        // Check file extension, if good, set filename
         if(!results?.files[0]?.name.includes('.xit')) throw 'Uploaded file is not *.xit format!';
+        setFileName(results?.files[0]?.name.split('.xit')[0]);
+
+        // Decode data and set path (if available), raw, and object
         const decodedData : string = atob(results?.files[0]?.data || null);
+
         setFilePath(results?.files[0]?.path || null); // TODO -> need more testing/verification on mobile
         setFileRaw(decodedData); // TODO -> need more testing/verification on mobile
         setFileObject(xitParse.default.toObject(decodedData)); // TODO -> need more testing/verification on mobile
@@ -129,6 +154,30 @@ const pickFileHandler = async (setFilePath: Function, setFileRaw: Function, setF
   }
 };
 
-// TODO -> file writing (manual and auto)
+const saveFileHandler = async (fileName: string, fileRaw: string) => {
+  // TODO -> electron and mobile versions of this. For now just use filesystem API
+  const options = {
+    suggestedName: fileName,
+    types: [
+      {
+        description: 'Xit files',
+        accept: {
+          'text/plain': ['.xit']
+        }
+      }
+    ]
+  };
+
+  try {
+    // ts-ignore needed for window global, unless we install types.
+    //@ts-ignore
+    const handle = await window.showSaveFilePicker(options);
+    const writable = await handle.createWritable();
+    await writable.write(fileRaw);
+    await writable.close();
+  } catch (e) {
+    console.error(`Unable to save file, reason: ${e}`);
+  }
+};
 
 export default App;
--- src/pages/raw/Raw.tsx
+++ src/pages/raw/Raw.tsx
@@ -1,13 +1,33 @@
-import { IonContent, IonPage} from '@ionic/react';
+import { IonContent, IonPage, IonTextarea} from '@ionic/react';
 import './raw.css';
 
-const Raw: React.FC = () => {
+// Utils (ts-ignore required for xit-parse for now)
+// @ts-ignore
+import * as xitParse from 'xit-parse';
+
+const Raw: React.FC<any> = (props: {fileName: string, fileRaw: string, setFileRaw: Function, setFileObject: Function}) => {
+  // TODO -> container formatting (I'd like to have it set in for example), margins
+  // TODO -> Would be nice to have line numbers, code font, syntax highlighting, etc. -> might not be wise to use ionic textarea? Think on it after getting basics done
   return (
-    <IonPage>
+    <IonPage id="raw-editor" >
       <IonContent fullscreen>
+        <h1>Raw Editor{props.fileName ? `: ${props.fileName}` : ''}</h1>
+        <IonTextarea value={(props.fileName && props.fileRaw) ? props.fileRaw : 'Use the button at top-right to open an Xit file.'} 
+                     disabled={!props.fileName}
+                     autoGrow 
+                     spellcheck
+                     debounce={200}
+                     onIonChange={(e) => handleIonChange(e, props.setFileRaw, props.setFileObject)}>
+        </IonTextarea>
       </IonContent>
     </IonPage>
   );
+};
+
+const handleIonChange = (event: any, setFileRaw: Function, setFileObject: Function) => {
+  const data = event.detail.value || '';
+  setFileRaw(data);
+  setFileObject(xitParse.default.toObject(data));
 };
 
 export default Raw;
